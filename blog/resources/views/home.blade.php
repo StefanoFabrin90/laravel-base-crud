4400efc41e3b4a3511daee3c9f0d8ac7194c8547
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-27 14:42:48 +0100

    first commit

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+    <section class="container">
+        <h1>welcome to comics</h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consectetur iste veniam recusandae nostrum dolore voluptatum, voluptates et earum esse distinctio, voluptate excepturi quo neque aut eius dolorum repellat porro?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consectetur iste veniam recusandae nostrum dolore voluptatum, voluptates et earum esse distinctio, voluptate excepturi quo neque aut eius dolorum repellat porro?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consectetur iste veniam recusandae nostrum dolore voluptatum, voluptates et earum esse distinctio, voluptate excepturi quo neque aut eius dolorum repellat porro?
+        </p>
+    </section>
+@endsection
---
+++ blog/resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="mt-5 text-center">
+    <p>comics for crud</p>
+</footer>
